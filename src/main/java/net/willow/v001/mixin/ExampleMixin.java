6d7b9955251da98d9b8a8076eb9afa4bed9d2483
SupmaCat <65302212+KaizoKat@users.noreply.github.com>
2023-01-04 13:46:36 +0100

    Initial commit

---
+++ src/main/java/net/willow/v001/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.willow.v001.mixin;
+
+import net.willow.v001.mod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		mod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
